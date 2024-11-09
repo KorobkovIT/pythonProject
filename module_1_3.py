80707ac06415c9b397a58978d4759851770c9bd7
Aleksandr Korobkov <korobkovperm523@gmail.com>
2024-11-09 20:20:21 +0500

    module_1_3

--- module_1_3.py
+++ module_1_3.py
@@ -1,3 +1,5 @@
+from ctypes import pythonapi
+
 name = 'Aleksandr'
 print(name)
 age = 33
@@ -10,3 +12,11 @@ print('Name: '+ name)
 print('Age: ' + str (age))
 print('New age: '+ new_age)
 print('Is Student: ' + str (is_student))
+
+
+homework = 12
+spent_time = 1.5
+course_name = 'Python'
+time_for_one_task = (spent_time/homework)
+
+print('Курс: '+ course_name+', всего задач: '+str (homework)+', затрачено часов: '+ str(spent_time)+', среднее время выполнения '+str(time_for_one_task)+' часа.')
