daf1ca00f7c76019a983210547335d09ea2261a8
Aleksandr Korobkov <korobkovperm523@gmail.com>
2024-11-09 19:07:45 +0500

    module_1_3

---
+++ module_1_3.py
@@ -0,0 +1,12 @@
+name = 'Aleksandr'
+print(name)
+age = 33
+print(age)
+new_age = '33'
+print(new_age)
+is_student = True
+print(is_student)
+print('Name: '+ name)
+print('Age: ' + str (age))
+print('New age: '+ new_age)
+print('Is Student: ' + str (is_student))
