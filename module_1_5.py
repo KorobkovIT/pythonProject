867ba6dcc515b10f340f9e729f1d3685cc8a4b5e
Aleksandr Korobkov <korobkovperm523@gmail.com>
2024-11-13 14:47:14 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,11 @@
+immutable_var = 1, 2, 'a', 'b'
+print(immutable_var)
+print(immutable_var[0])
+#immutable_var[0] = 3
+#tuple (кортеж) - для хранения данных в неизменном виде
+
+mutable_list = [1, 2, 'a', 'b']
+print(mutable_list)
+print(mutable_list[0])
+mutable_list[0]=3
+print(mutable_list)
