a72307d3892e39295f621bf575ccf1906167bfaf
Aleksandr Korobkov <korobkovperm523@gmail.com>
2024-11-26 21:34:28 +0500

    26/11

--- 1.py
+++ 1.py
@@ -1,13 +1,20 @@
-name = 'Chris'
-food = 74
-print(f'Hello. My name is {name} and I like {food}.')
-print(f'Hello. My name is {name} and I like {food}.')
-a = 7
-k = 0
-for i in range(2, a // 2+1):
-    if (a % i == 0):
-        k = k+1
-if (k <= 0):
-    print("Число простое")
-else:
-    print("Число не является простым")
+# создаём функцию send_email
+def send_email(message,recipient,sender = "univerity.help@gmail.com"):
+    if("@"and(".com"or".ru"or".net")) not in (recipient or sender) or("@" or(".com"or"ru"or".net")) not in (recipient or sender):
+        # создаём  1 условие
+        print(f"Невозможно отправить письмо с адреса {sender}на адрес {recipient}")
+    elif recipient == sender:
+        # создём 2 условие
+        print("Нельзя отправить письмо самому себе!")
+        # создаём 3 условие
+    elif sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса{sender} на адрес {recipient}.")
+        # создаём 4 условие
+    elif sender !="university.help@gmail.com":
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+        # вывод результата
+send_email('messagje','university.help@mail.com','@.com')
+send_email('messagje','university.help@mail.com')
+send_email('messagje','universi@.com')
+send_email('messagje','universi@.co')
+send_email('messagje','universi@.com')
