4d6b08f3a06bf00fc5351dfd206271d531ae7896
Aleksandr Korobkov <korobkovperm523@gmail.com>
2024-12-02 23:08:22 +0500

    02.12

--- 1.py
+++ 1.py
@@ -1,3 +1,16 @@
-my_list = [1, 2, 3, 2, 4, 2, 5]
-count_of_twos = my_list.count(2)
-print(count_of_twos)
+def get_multiplied_digits(number):
+    number = int(number)
+    str_number = str(number)
+    first = int(str_number[0])
+    while str_number.endswith("0"):
+        str_number = str_number[:len(str_number)-1]
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+
+result = get_multiplied_digits(40203)
+print(result)
+
+result2 = get_multiplied_digits(402030)
+print(result2)
