a5a8913ebf197bb741aa972c0fed1fd806161594
Aleksandr Korobkov <korobkovperm523@gmail.com>
2024-12-30 17:48:57 +0500

    30.12

--- 1.py
+++ 1.py
@@ -1,7 +1,3 @@
-my_list = ["dog", 23, "cat",
-41, "fish"]
-for elem in my_list:
-    if isinstance(elem, str):
-        print(elem.upper())
-    else:
-        print(elem)
+my_list = ["dog", 'HHGVF', "cat", 'gfgHHG', "Fish"]
+lej = map(str.lower, my_list)
+print(list(lej))
