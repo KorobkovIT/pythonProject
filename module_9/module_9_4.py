0087cfe2b9ca53058937a8435f4a7a3909ecf1a5
Aleksandr Korobkov <korobkovperm523@gmail.com>
2025-01-28 19:22:44 +0500

    new str

--- module_9/module_9_4.py
+++ module_9/module_9_4.py
@@ -3,12 +3,15 @@
 
 Задача "Функциональное разнообразие"ий-генераторов.
 """
+from random import choice
 
+#Lambda-функция:
 first = 'Мама мыла раму'
 second = 'Рамена мало было'
 result = list(map(lambda x, y: x == y, first, second))
 print(result)
 
+#Замыкание:
 def get_advanced_writer(file_name):
     def write_everything(*data_set):
         with open(file_name, 'a', encoding='utf-8') as file:
@@ -19,6 +22,7 @@ def get_advanced_writer(file_name):
 write = get_advanced_writer('example.txt')
 write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
 
+#Метод __call__:
 class MysticBall:
     def __init__(self, *words):
         self.words = words
