9052e9017654d92eb8cef9d57fe10cd9c7dcb576
Aleksandr Korobkov <korobkovperm523@gmail.com>
2025-01-31 14:56:57 +0500

    Update module_10_1.py

--- module_10/module_10_1.py
+++ module_10/module_10_1.py
@@ -3,18 +3,21 @@
 
 Задача "Потоковая запись в файлы"
 """
+import time
 from time import sleep
 import threading
 
 def wite_words(word_count, file_name):  #word_count - количество записываемых слов, file_name - название файла, куда будут записываться слова.
     n = 0
+    stat = time.time()
     with open(file_name, 'a', encoding='utf-8') as file:
         while n != word_count:
             n += 1
             file.write(f'Какое-то слово № {n}\n')
-            time.sleep(0.1)
+            sleep(0.1)
     print(f'Завершилась запись в файл{file_name}')
-
+    fin = time.time()
+    print(fin-stat)
 
 
 wite_words(10, 'example1.txt')
